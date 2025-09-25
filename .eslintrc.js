e2a49daf9f0199a1ce32499fb0d602ed4ea4617d
Gabriel Oliveira <gabrieloliveirawebsite@gmail.com>
2025-09-25 10:06:15 -0300

    ajustes feitos no versel

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    'no-console': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
